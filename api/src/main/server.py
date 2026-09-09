00aef9c9e8585bc1a8d2060d1da5605383e8e743
vitor-gblop <vitor.gblop@gmail.com>
2026-09-09 10:51:07 -0500

    [feat] ativação do cors

--- api/src/main/server.py
+++ api/src/main/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # Routes
 from src.main.routes.pet_router import pet_router
 from src.main.routes.documents_router import document_router 
@@ -11,6 +12,13 @@ Base.metadata.drop_all(bind=engine)  # drop all tables if they exist
 Base.metadata.create_all(bind=engine) # create tables 
 
 app = FastAPI(title="vetglobal", version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routers
 app.include_router(pet_router)
